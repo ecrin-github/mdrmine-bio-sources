25774565c0a7ba0b11365cbd7740975dce91510e
cudillal <leopold.cudilla@ecrin.org>
2024-09-18 10:31:54 +0000

    Add WHO headers file path as property in mdrmine project.xml

--- who/src/main/java/org/intermine/bio/dataconversion/WhoConverter.java
+++ who/src/main/java/org/intermine/bio/dataconversion/WhoConverter.java
@@ -12,6 +12,7 @@ package org.intermine.bio.dataconversion;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.io.File;
 import java.io.Reader;
 import java.io.BufferedReader;
 import java.nio.file.Files;
@@ -33,17 +34,16 @@ import com.opencsv.ICSVWriter;
 import com.opencsv.exceptions.CsvMalformedLineException;
 
 
-
 /**
  * 
  * @author
  */
 public class WhoConverter extends BioFileConverter
 {
-    // TODO: to somewhere else?
+    // TODO: to somewhere else? headers file
     private static final String DATASET_TITLE = "ICTRPWeek22July2024";
     private static final String DATA_SOURCE_NAME = "WHO";
-    private static final String HEADERS_FILE_PATH = "/home/ubuntu/data/who/ICTRP_headers.csv";
+    private String headersFilePath = "";
 
     /**
      * Constructor
@@ -54,14 +54,20 @@ public class WhoConverter extends BioFileConverter
         super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE);
     }
 
+    public void setHeadersFilePath(String fp) {
+        /* headers.filePath property in project.xml */
+        this.headersFilePath = fp;
+    }
+
     public void process(Reader reader) throws Exception {
-        final Map<String, Integer> fieldsToInd = this.getHeaders(HEADERS_FILE_PATH);
+        /* Opened BufferedReader is passed as argument (from FileConverterTask.execute()) */
+
+        final Map<String, Integer> fieldsToInd = this.getHeaders();
 
         final CSVParser parser = new CSVParserBuilder()
                                         .withSeparator(',')
                                         .withQuoteChar('"')
                                         .build();
-        // final CSVParser parser = new CSVParserBuilder().withSeparator(',').withIgnoreQuotations(true).build();
         final CSVReader csvReader = new CSVReaderBuilder(reader)
                                             .withCSVParser(parser)
                                             .build();
@@ -85,6 +91,8 @@ public class WhoConverter extends BioFileConverter
                 skipNext = true;
             }
         }
+
+        /* BufferedReader is closed in FileConverterTask.execute() */
     }
 
     public void storeValues(Map<String, Integer> fieldsToInd, String[] values) throws Exception {
@@ -102,7 +110,6 @@ public class WhoConverter extends BioFileConverter
         if (!trialID.isEmpty()) {
             studyIdentifier.setAttribute("identifierValue", trialID);
         }
-        studyIdentifier.setAttribute("source", "source_example_value");
         studyIdentifier.setReference("study", study);
         store(studyIdentifier);
 
@@ -176,118 +183,17 @@ public class WhoConverter extends BioFileConverter
         return true;
     }
 
-    /**
-     * 
-     *
-     * {@inheritDoc}
-     */
-    // public void processzzzzzzzzzzzzzzzzzzz(Reader reader) throws Exception {
-    //     /* Opened BufferedReader is passed as argument (from FileConverterTask.execute()) */
-        
-    //     String line;
-    //     String[] values;
-    //     Item study;
-    //     Boolean firstCol = true;
-    //     Boolean strFinished = true;
-    //     Boolean startNewline = false;
-    //     StringBuilder sb;
-    //     int c;
-    //     int quotes = 0;
+    public Map<String, Integer> getHeaders() throws Exception {
+        if (this.headersFilePath.equals("")) {
+            throw new Exception("headersFilePath property not set in mdrmine project.xml");
+        }
 
-    //     Map<String, Integer> fieldsToInd = this.getHeaders(HEADERS_FILE_PATH);
+        if (!(new File(this.headersFilePath).isFile())) {
+            throw new Exception("WHO Headers file does not exist (path tested: " + this.headersFilePath + " )");
+        }
 
-    //     if (fieldsToInd != null) {
-    //         BufferedReader br = new BufferedReader(reader);
-    //         // while ((line = br.readLine()) != null) {
-    //         while ((c = br.read()) != -1) {
-    //             /* Most values are delimited by triple quotes ("""[value]""")
-    //                exceptions are ID (1st field), NULL values, and dates (AFAIK) */
-    //             // TODO: need to do a custom parsing, careful of empty values ("""""")
-    //             // TODO: use external csv library (this is nonsense) or look how MDR parses it in case
-    //             c = (char) c;
-                
-    //             if (startNewline && c == "n") {
-    //                 startNewline = false;
-
-    //             } else {
-    //                 if (strFinished) {
-    //                     if (c == "\"") {    // Quotes
-    //                         if (quotes == 3) {  // TODO: could break with """"""" (7 quotes) value
-    //                             // Adding empty value ("""""")
-    //                             values.append("");
-    //                             quotes = 0;
-    //                         }
-    //                         quotes++;
-    //                     } else if (c == ",") {  // Separator (comma)
-    //                         quotes = 0;
-    //                     } else if (c == "\\") {  // Might be newline
-    //                         startNewline = true;
-    //                     } else {    // Non-quotes/comma character found
-    //                         strFinished = false;
-    //                         if (quotes == 3) {  // End of the first 3 quotes surrounding the value
-    //                             quotes = 0;
-    //                             // Adding previous value to list of values
-    //                             values.append(sb);
-    //                             // Beginning new value string
-    //                             sb = new StringBuilder(c);
-    //                         } else {    // 1 or 2 quotes are in the middle of the value
-    //                             while (quotes > 0) {
-    //                                 sb.append("\"");
-    //                                 quotes--;
-    //                             }
-    //                             sb.append(c);
-    //                         }
-    //                     }
-    //                 } else {
-    //                     if (c != "\"") {
-    //                         sb.append(c);
-    //                     } else {
-    //                         quotes++;
-    //                         strFinished = true;
-    //                     }
-    //                 }
-    //             }
-
-    //             study = createItem("Study");
-    //             String displayTitle = values[fieldsToInd.get("public_title")].strip();
-    //             if (!displayTitle.isEmpty()) {
-    //                 study.setAttribute("displayTitle", displayTitle);
-    //             } else {
-    //                 study.setAttribute("displayTitle", "Unknown study title");
-    //             }
-    //             Item studyIdentifier = createItem("StudyIdentifier");
-    //             String trialID = values[fieldsToInd.get("TrialID")].strip();
-    //             if (!trialID.isEmpty()) {
-    //                 studyIdentifier.setAttribute("identifierValue", trialID);
-    //             }
-    //             studyIdentifier.setAttribute("source", "source_example_value");
-    //             studyIdentifier.setReference("study5", study);
-    //             store(studyIdentifier);
-
-    //             Item studyTitle = createItem("StudyTitle");
-    //             String titleText = values[fieldsToInd.get("Scientific_title")].strip();
-    //             if (!titleText.isEmpty()) {
-    //                 studyTitle.setAttribute("titleText", titleText);
-    //             }
-    //             studyTitle.setReference("study11", study);
-    //             store(studyTitle);
-
-    //             // Study collections
-    //             study.addToCollection("studyIdentifiers", studyIdentifier);
-    //             study.addToCollection("studyTitles", studyTitle);
-    //             store(study);
-    //         }
-    //     } else {
-    //         throw new Exception("Failed to parse header file");
-    //     }
-
-    //     /* BufferedReader is closed in FileConverterTask.execute() */
-    // }
-
-    public Map<String, Integer> getHeaders(String headersFilePath) throws Exception {
-        // TODO: to some config file somewhere?
-        List<String> fileContent = Files.readAllLines(Paths.get(headersFilePath), StandardCharsets.UTF_8);
-        Map<String, Integer> fieldsToInd = null;
+        List<String> fileContent = Files.readAllLines(Paths.get(this.headersFilePath), StandardCharsets.UTF_8);
+        Map<String, Integer> fieldsToInd = new HashMap<String, Integer>();
 
         if (fileContent.size() > 0) {
             String headersLine = String.join("", fileContent).strip();
@@ -296,7 +202,6 @@ public class WhoConverter extends BioFileConverter
                 headersLine = headersLine.substring(1);
             }
             String[] fields = headersLine.split(",");
-            fieldsToInd = new HashMap<String, Integer>();
             
             for (int ind = 0; ind < fields.length; ind++) {
                 fieldsToInd.put(fields[ind], ind);
