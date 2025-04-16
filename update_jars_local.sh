1d32fc1d49066ace7b5f3bc21678c321de03ba8c
cudillal <leopold.cudilla@ecrin.org>
2025-04-16 08:57:44 +0000

    Modify local JARs update script to move intermine JARs as well

--- update_jars_local.sh
+++ update_jars_local.sh
@@ -1,6 +1,6 @@
 ./gradlew clean
 ./gradlew install --stacktrace
 rm -rf ~/code/mdrmine/sources_jars/*
-cp -r ~/.m2/repository/org/intermine/bio* ~/code/mdrmine/sources_jars/
+cp -r ~/.m2/repository/org/intermine/* ~/code/mdrmine/sources_jars/
 # Commons
-cp -r ~/.m2/repository/org/intermine/commons ~/code/mdrmine/sources_jars/
+# cp -r ~/.m2/repository/org/intermine/commons ~/code/mdrmine/sources_jars/
